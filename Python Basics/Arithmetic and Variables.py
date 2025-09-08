af495e5d400d8ce5cd340dc0ab723d69a54091d5
Luke Papaevangeliou <lukepapa.work@gmail.com>
2025-09-08 18:57:31 +1000

    Create Arithmetic and Variables.py
    Feel great getting a start on my learning journey today. I'm looking forward to what will come tomorrow.

---
+++ Python Basics/Arithmetic and Variables.py
@@ -0,0 +1,29 @@
+# Printing a simple message
+print("Hello, World!")
+
+# Printing arithmetic operations
+print("5 + 3 =", 5 + 3)
+print("10 - 2 =", 10 - 2)
+print("4 * 7 =", 4 * 7)
+print("20 / 4 =", 20 / 4)
+print("2 ** 3 =", 2 ** 3)
+
+# Controlling order of operations with parentheses
+print("5 + 3 * 2 =", 5 + 3 * 2)
+print("(5 + 3) * 2 =", (5 + 3) * 2)
+
+# Creating Variables
+a = 10
+b = 5
+sum_ab = a + b
+print("The sum of a and b is:", sum_ab)
+
+# Manipulating Variables
+a += 2
+b *= 3
+print("Updated a:", a)
+print("Updated b:", b)
+
+# Using Multiple Variables
+x, y, z = 1, 2, 3
+print("Values of x, y, z:", x, y, z)
